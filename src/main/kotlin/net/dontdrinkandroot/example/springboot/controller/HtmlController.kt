eea3d70e0db2576b78a0b6b4d6c185158c51db93
Philip Washington Sorst <philip@sorst.net>
2019-03-01 21:53:29 +0100

    Initial commit

---
+++ src/main/kotlin/net/dontdrinkandroot/example/springboot/controller/HtmlController.kt
@@ -0,0 +1,17 @@
+package net.dontdrinkandroot.example.springboot.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HtmlController {
+
+    @GetMapping("/")
+    fun index(model: Model): String {
+        model["title"] = "Hello World"
+        return "index";
+    }
+
+}
